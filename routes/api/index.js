86e20fac326f236693ed28abc9e226974531653b
sudan bhusal <sudanbhusal20215@gmail.com>
2021-03-15 16:25:34 -0700

    small change

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,5 @@
 const router = require("express").Router();
 const bookRoutes = require("./books");
-const apiRoutes = require("./api");
-router.use("/api", apiRoutes);
 
 // Book routes
 router.use("/books", bookRoutes);
