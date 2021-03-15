eeeb08d8adeccd9e6a3053bf5847021d50fe1401
sudan bhusal <sudanbhusal20215@gmail.com>
2021-03-15 16:00:17 -0700

    client

--- server.js
+++ server.js
@@ -36,6 +36,9 @@ MongoClient.connect(url, { useUnifiedTopology: true })
   if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client"));
+  });
 
 
 app.listen(PORT, () => {
